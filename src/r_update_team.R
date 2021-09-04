955e1ac7eb1fd02c920bdc92d42046e96d728987
Alex Buskirk <alex.buskirk@outlook.com>
2021-09-04 12:40:05 -0700

    adding team update

---
+++ src/r_update_team.R
@@ -0,0 +1,61 @@
+# SETUP ----
+
+## For Task Scheduler  ----
+setwd('C:/Users/catspur/Desktop/projects/MLB-ETL')
+
+## Load functions and libraries ----
+source(
+  paste(
+    getwd(),
+    '/src/',
+    'r_functions.R',
+    sep = ''
+  )
+)
+
+function_load_libraries()
+
+## Create connection ----
+function_odbc_set_up()
+
+## Logging operations ----
+operations_number <- dbGetQuery(
+  conn = odbc_connection_pc,
+  'select max(operation_id) from mlb.dbo.time_tracking'
+) %>% as.numeric()
+
+operation_start_dtm <- Sys.time()
+operation_name <- 'update_schedule'
+operation_id <- operation_id + 1
+
+## Create log file ----
+log_file <- file(
+  paste(
+    getwd(),
+    '/data/log/mlb_schedule_update_log/',
+    str_replace_all(
+      Sys.time(),
+      '-| |:',
+      '_'
+    ),
+    '.txt',
+    sep = ''
+  ),
+  open = 'a'
+)
+
+## Start Operations ----
+
+team_table <- read.csv('data/team_table.csv') %>%
+  mutate(operation_id = operation_id,
+         last_update_dtm = operation_start_dtm)
+
+dbWriteTable(
+  odbc_connection_pc,
+  value = team_table,
+  name = 'team',
+  append = TRUE#,
+  #filed.type = class_type
+)
+
+function_logging()
